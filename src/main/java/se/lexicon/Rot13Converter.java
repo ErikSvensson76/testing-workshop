9a42b5f16f876515f243b8ac992f0e31a5e824a9
Erik Svensson <eosvensson@gmail.com>
2020-04-14 16:42:51 +0200

    Uploaded workshop

---
+++ src/main/java/se/lexicon/Rot13Converter.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+
+public class Rot13Converter {
+
+    public static char convertChar(char source) {
+        return 0;
+    }
+
+    public static String convertText(String source) {
+        return "";
+    }
+}
---
+++ src/test/java/se/lexicon/Rot13ConverterTest.java
@@ -0,0 +1,91 @@
+package se.lexicon;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Rot13ConverterTest {
+    @Test
+    public void test_a_becomes_n(){
+        //Arrange
+        char source = 'a';
+        char expected = 'n';
+
+        //Act
+        char actual = Rot13Converter.convertChar(source);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    /*
+    @Test
+    public void test_n_becomes_a(){
+        char source = 'n';
+        char expected = 'a';
+
+        char actual = Rot13Converter.convertChar(source);
+        assertEquals(expected,actual);
+    }
+    */
+
+    /*
+    @Test
+    public void test_Z_becomes_M(){
+        char source = 'Z';
+        char expected = 'M';
+        char actual = Rot13Converter.convertChar(source);
+        assertEquals(expected,actual);
+    }
+     */
+
+    /*
+    @Test
+    public void test_A_becomes_N(){
+        char source = 'A';
+        char expected = 'N';
+        char actual = Rot13Converter.convertChar(source);
+        assertEquals(expected,actual);
+    }
+     */
+
+    /*
+    @Test
+    public void test_Ö_stays_Ö(){
+        char source = 'Ö';
+        char expected = 'Ö';
+        char actual = Rot13Converter.convertChar(source);
+        assertEquals(expected, actual);
+    }
+     */
+
+    /*
+    @Test
+    public void test_java_becomes_wnin(){
+        String source = "java";
+        String expected = "wnin";
+        String actual = Rot13Converter.convertText(source);
+        assertEquals(expected, actual);
+    }
+     */
+
+    /*
+    @Test
+    public void test_wnin_becomes_java(){
+        String source = "wnin";
+        String expected = "java";
+        String actual = Rot13Converter.convertText(source);
+        assertEquals(expected,actual);
+    }
+     */
+
+    /*
+    @Test
+    public void test_Hello_World_becomes_Uryyb_Jbeyq(){
+        String source = "Hello World!";
+        String expected = "Uryyb Jbeyq!";
+        String actual = Rot13Converter.convertText(source);
+        assertEquals(expected, actual);
+    }
+     */
+}
